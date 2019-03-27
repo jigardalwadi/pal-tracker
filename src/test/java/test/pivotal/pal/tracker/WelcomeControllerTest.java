1c711ba4cdcf99074271e4f06c989121a4b57f1f
jigar <jigar.dalwadi@perficient.com>
2019-03-27 11:27:17 -0700

    controller added

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -1,9 +1,8 @@
 package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
 
 public class WelcomeControllerTest {
 
